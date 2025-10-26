9edfe3b0818d2f6f8424aa83cb8070e1ca5ebe11
suraj-mali-1423 <surajsmali@coep.sveri.ac.in>
2025-10-26 08:59:19 +0530

    SVG Animation Concept.

---
+++ SVGAnimation/script.js
@@ -0,0 +1,25 @@
+var path = `M 10 100 Q 250 150 490 100`;
+
+var finalPath = `M 10 100 Q 250 100 490 100`;
+
+var string = document.querySelector("#string")
+
+string.addEventListener("mousemove" , (e) => {
+        path =  `M 10 100 Q ${e.x} ${e.y} 490 100`
+        // console.log(path);
+        gsap.to("svg path" , {
+           attr:{d:path},
+           duration:0.3,
+           ease:"power3.out"
+        })
+})
+
+
+string.addEventListener("mouseleave" , () => {
+        gsap.to("svg path" , {
+           attr:{d:finalPath},
+           duration:1.5,
+           ease:"elastic.out(1,0.2)"
+        })
+})
+
